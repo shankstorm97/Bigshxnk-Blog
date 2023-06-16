01acf095ee91e7ba28875c16979e10753d519884
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-06-16 23:19:36 +0530

    commit before starting project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,43 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { Inter } from "next/font/google";
+import { Tilt_Prism } from "next/font/google";
+import { Bodoni_Moda } from "next/font/google";
+import { Oswald } from "next/font/google";
+import Image from "next/image";
+import Man from "../man.jpg";
 
-const inter = Inter({ subsets: ['latin'] })
+// import styles from "./page.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const tiltPrism = Tilt_Prism({ subsets: ["latin"] });
+const cinzel = Bodoni_Moda({ subsets: ["latin"] });
+const oswald = Oswald({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <main className="container">
+        <div className="container py-5 px-5 mx-auto">
+          <div className={cinzel.className}>
+            <div style={{ fontSize: 50 }}>
+              Why "The way of superior man" is a must read...
+            </div>
+
+            <div className={oswald.className}>
+              <div style={{ fontSize: 30 }}>
+                We all know how men are becoming man-child nowadays.
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </main>
+      <Image
+        src={Man}
+        height={2000}
+        alt="Man"
+        width={2000}
+        style={{ borderRadius: 20 }}
+        className="flex container mx-auto"
+      />
+    </>
+  );
 }
