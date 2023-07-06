73db46aa0f4ee6386cd65eeb7b795e88ce9bbdf8
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-07-07 02:15:06 +0530

    commit for gsap trigger malfunction

--- src/components/landingPageComponents/HeroCardBig.tsx
+++ src/components/landingPageComponents/HeroCardBig.tsx
@@ -2,6 +2,7 @@
 import { gsap } from "gsap";
 import React, { useRef, useEffect } from "react";
 import { ScrollTrigger } from "gsap/all";
+import { Power0 } from "gsap";
 
 const HeroCardBig = () => {
   const heroCardBigRef = useRef(null);
@@ -11,23 +12,20 @@ const HeroCardBig = () => {
   gsap.registerPlugin(ScrollTrigger);
   useEffect(() => {
     gsap.to(heroCardBigRef.current, {
-      y: 200,
-      opacity: 0,
-      duration: 3,
+      y: -10,
+      duration: 0.1,
       scrollTrigger: {
         trigger: heroCardBigRef.current,
+        toggleActions: "restart none none none",
       },
     });
   }, []);
 
   return (
     <>
-      <div
-        className="h-[64rem] w-full m-auto bg-gradient-to-b from-neutral-600 to-black"
-        ref={heroCardBigRef}
-      >
+      <div className="h-[64rem] w-full m-auto bg-gradient-to-b from-neutral-600 to-black">
         <div className="max-w-[95%] bg-neutral-500 m-auto w-full h-full flex">
-          ;pokijuhgv
+          <h1 ref={heroCardBigRef}>This is the reference heading.</h1>
         </div>
       </div>
     </>
