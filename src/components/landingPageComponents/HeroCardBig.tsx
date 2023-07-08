93faa8d94376936ed250bf84bdf2b53c2d5b9d61
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-07-08 23:06:42 +0530

    commit for landing page

--- src/components/landingPageComponents/HeroCardBig.tsx
+++ src/components/landingPageComponents/HeroCardBig.tsx
@@ -26,9 +26,9 @@ const HeroCardBig = () => {
   return (
     <>
       {/* <div className="h-[64rem] w-full m-auto bg-gradient-to-b from-neutral-600 to-black flex box-border"> */}
-      <div className="h-[64rem] w-full m-auto bg-white flex box-border">
-        <div className="max-w-[95%] w-full bg-white h-2/3 rounded-2xl basis-3/5 p-4 m-0 box-border">
-          <div className="relative h-full w-full bg-gray-900 box-border rounded-[15px]">
+      <div className="h-[100vh] w-full m-auto bg-white flex box-border">
+        <div className="max-w-[100%] w-full bg-white h-2/3 rounded-2xl basis-3/5 p-4 m-0 box-border">
+          <div className="relative h-full w-3/4 bg-gray-900 box-border rounded-[15px]">
             <Image
               src={Lamp}
               alt="lamp-image"
@@ -41,22 +41,24 @@ const HeroCardBig = () => {
               className="flex justify-start align-middle z-10 pr-4 pl-4 bg-white rounded-br-[15px] w-fit text-4xl after:content-[''] after:absolute after:left-[100%] after:h-1/2 after:rounded-tl-2xl after:shadow-heroCardBigAfter after:w-full box-border after:z-40 h-fit min-w-[40%] absolute top-0 m-auto"
             >
               <div className="flex justify-start align-bottom">
-                My name is prabhdeep singh
+                Will this influencer-s**t stop ?
               </div>
             </div>
             <div
               ref={heroCardBigRef}
-              className="flex justify-start align-middle z-10 pr-4 pl-4 bg-white rounded-br-[15px] w-fit text-4xl after:content-[''] after:absolute after:left-[100%] after:h-1/2 after:rounded-tl-2xl after:shadow-heroCardBigAfter after:w-full box-border after:z-40  absolute top-10 h-10"
+              className="flex justify-start align-middle z-10 pr-4 pl-4 bg-white rounded-br-[15px] w-fit text-3xl after:content-[''] after:absolute after:left-[100%] after:h-1/2 after:rounded-tl-2xl after:shadow-heroCardBigAfter after:w-full box-border after:z-40  absolute top-10 h-10"
             >
-              <div className="flex justify-start align-bottom">
+              <div className="flex justify-start font-serif font-extrabold align-bottom">
                 Prabhdeep singh
               </div>
             </div>
             <div
               ref={heroCardBigRef}
-              className="flex justify-start align-middle z-10 pr-4 pl-4 bg-white rounded-br-[15px] w-fit text-4xl after:content-[''] after:absolute after:left-[100%] after:h-1/2 after:rounded-tl-2xl after:shadow-heroCardBigAfter after:w-full box-border after:z-40 h-10  absolute top-[5rem]"
+              className="flex justify-start align-middle z-10 pr-4 pl-4 bg-white rounded-br-[15px] w-fit text-2xl after:content-[''] after:absolute after:left-[100%] after:h-1/2 after:rounded-tl-2xl after:shadow-heroCardBigAfter after:w-full box-border after:z-40 h-10  absolute top-[5rem]"
             >
-              <div className="flex justify-start align-bottom">Prabhdeep</div>
+              <div className="flex justify-start align-bottom font-serif font-extrabold">
+                Lifestyle
+              </div>
             </div>
           </div>
         </div>
