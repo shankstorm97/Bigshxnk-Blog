3e9bd381957b7a8fa6d95f8a2f4c4def645c9f6c
shankstorm <work.prabhdeepsinngh@gmail.com>
2023-07-27 22:25:53 +0530

    commit for image tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,8 +61,8 @@ export default async function Home() {
           <Image
             src={`https:${coverImageUrl}`}
             alt="Blog-of-the-week-image"
-            width={1200}
-            height={1200}
+            width={1500}
+            height={1500}
             className="rounded-2xl  hover:scale-110 duration-700 transition ease-in-out object-contain "
           />
           <div className="absolute top-1/3 left-3/6 w-full">
